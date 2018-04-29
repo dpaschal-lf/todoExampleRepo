4d4d1ca9d2a27ca90ba095a3c0fef45c71744203
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-04-29 16:15:33 -0700

    added dummy skeleton functions

---
+++ includes/main.js
@@ -0,0 +1,29 @@
+
+
+$(document).ready( initializeApp );
+
+function initializeApp(){
+
+}
+/* create a single dom section for a single todo task*/
+/* input: task object
+output: task dom element to attach to page
+*/
+function createTaskItem(){
+
+}
+
+/* render all tasks to the dom, clear the dom first, then render all
+input: array of tasks
+output: none
+*/
+function renderAllTasks(){
+
+}
+/* get all task data from resources and then render tasks to dom
+input: none
+output: none
+*/
+function getTaskData(){
+
+}
