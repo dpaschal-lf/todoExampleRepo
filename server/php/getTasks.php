265b42e8863e739ed1280769a03da8218c095109
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-04-30 14:52:20 -0700

    added read of all records and 1 record in particular

--- server/php/getTasks.php
+++ server/php/getTasks.php
@@ -1,5 +1,40 @@
 <?php
 
+require_once('mysql_credentials.php');
 
+$query = "SELECT * FROM tasks";
+
+if(!empty($_GET['id'])){
+	$_GET['id'] = addslashes($_GET['id']);
+	$query .= " WHERE ID='{$_GET['id']}'";
+}
+$result = mysqli_query($conn, $query);
+$output = [
+	'success'=> false, 
+	'tasks' => [], 
+	'user' => [],
+	'errors'=> []
+];
+
+if($result){
+	//query was fine
+	if(mysqli_num_rows($result)>0){
+		//query returned data
+		$output['success'] = true;
+		while($row = mysqli_fetch_assoc($result)){
+			$output['tasks'][] = $row;
+		}
+	} else {
+		//there was no data in the query
+		$output['errors'][] = 'no data available';
+	}
+} else {
+	//mysql problem
+	$output['errors'][] = 'error with query';
+}
+
+$json_output = json_encode($output);
+
+print($json_output);
 
 ?>
